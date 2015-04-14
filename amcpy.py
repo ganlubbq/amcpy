32eb89c10c06775fd6fe1f4436a55849f5e629cb
Zhechen Zhu <zhechen.zhu@gmail.om>
2015-04-14 01:14:49 +0100

    Complete ML Classifier

--- amcpy.py
+++ amcpy.py
@@ -1,14 +1,14 @@
 import numpy as np
-
+import amcstat
+import amcgen
 def amcml(sigIn,modulationPool,channelParameter):
     "AMCML classifies the modulation type of the input signal using the maxium\
             likelihood classifier."
-    
-
-    :sigIn: TODO
-    :modulationPool: TODO
-    :channelParameter: TODO
-    :returns: TODO
-
-    """
-    pass
+    likelihood = np.zeros(len(modulationPool))
+    for iModulation in range(len(modulationPool)):
+        symbol = amcgen.getsymbol(modulationPool[iModulation])
+        sigma = np.sqrt(10**(-channelParameter/10))/np.sqrt(2)
+        likelihood[iModulation]=amcstat.likelihood(sigIn, symbol,\
+                sigma)
+    decision = np.argmax(likelihood)
+    return decision
--- amcstat.py
+++ amcstat.py
@@ -5,11 +5,13 @@ def likelihood(signal,alphabet,sigma):
     "Likelihood Function Calculate the log likelihood of signal belonging to a\
             set of bivariate normal distributions with specified\
             alphabet(means). and sigma(standard deviation)."
-    for iSignal in signal.size:
-        for iAlphabet in alphabet.size:
-            iLikelihood(iAlphabet) = np.exp(-ny.power(np.abs(signal[iSignal]\
-                    -alphabet[iAlphabet]),2)/2/np.power(sigma,2))/(2*pi*\
-                    np.power(sigma,2))
-        likelihood(iSignal) = np.mean(iLikelihood)
-    
+    iLikelihood = np.zeros(alphabet.shape[1])
+    likelihood = np.zeros(signal.shape[1])
+    for iSignal in range(signal.shape[1]):
+        for iAlphabet in range(alphabet.shape[1]):
+            iLikelihood[iAlphabet] = np.exp(-np.power(np.abs(signal[0,iSignal]\
+                   -alphabet[0,iAlphabet]),2)/2/sigma**2)/(2*np.pi*\
+                    sigma**2)
+        likelihood[iSignal] = np.mean(iLikelihood)
     likelihood = np.sum(np.log(likelihood))
+    return likelihood
