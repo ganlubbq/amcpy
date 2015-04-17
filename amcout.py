3989e39390c945c40752924e12612dd74ca62e70
Zhechen Zhu <zhechen.zhu@gmail.om>
2015-04-17 22:15:55 +0100

    amcout.py for graphical out and etc.

---
+++ amcout.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+def constplot(signal,title):
+    "plot signal constellation on a two-dimentional plane" 
+    
+    # Initialize markers
+    marker = ['o', 'x', 's', 'D']
+    color = ['k', 'b', 'r', 'g']
+    
+    # Define plot figure size
+    fig=plt.figure(figsize=(8, 8),dpi=100)
+    for iSignal in range(signal.shape[0]):
+        plt.plot(signal[iSignal,:].real,signal[iSignal,:].imag,linestyle='',marker=marker[iSignal] ,color=color[iSignal], mfc='none')
+    
+    plt.title(title)
+    plt.xlabel('In-phase Component')
+    plt.ylabel('Quadrature Component')
+    
+    plt.show()
